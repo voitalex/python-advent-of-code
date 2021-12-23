162d787a88811e1c37103a63ec253689e0d3dbea
aavojtek <aavojtek@mts.ru>
2021-12-23 17:00:32 +0300

    Year 2021, day 07

---
+++ tests/y2021/test_d07.py
@@ -0,0 +1,42 @@
+""" Day 07: The Treachery of Whales """
+
+import pytest
+from advent_of_code.y2021.d07 import first_task, second_task
+from tests.consts import Task
+
+
+@pytest.mark.y2021d07
+class TestDay07:
+    """ Набор тестов для задач 7-ого дня """
+
+    DAY = 7
+
+    @pytest.mark.parametrize(
+        'values, expected',
+        [
+            (['1,2'], 1),
+            (['1,2,4'], 3),
+            (['16,1,2,0,4,2,7,1,2,14'], 37),
+
+        ]
+    )
+    def test_first_task_oneliners(self, values, expected):
+        assert first_task(values) == expected
+
+    def test_first_task_from_file(self, y2021_file_loader):
+        assert first_task(y2021_file_loader(self.DAY, Task.first)) == 344605
+
+    @pytest.mark.parametrize(
+        'values, expected',
+        [
+            (['1,2'], 1),
+            (['1,2,4'], 4),
+            (['16,1,2,0,4,2,7,1,2,14'], 168),
+
+        ]
+    )
+    def test_second_task_oneliners(self, values, expected):
+        assert second_task(values) == expected
+
+    def test_second_task_from_file(self, y2021_file_loader):
+        assert second_task(y2021_file_loader(self.DAY, Task.second)) == 93699985
