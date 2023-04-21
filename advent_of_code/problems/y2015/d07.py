96da1ac2bb9b1f239fb54088bd4d00718e27add0
vtb4059174 <vtb4059174@vtb.ru>
2023-04-21 18:38:53 +0300

    Y2015D07: Рефакторинг

--- advent_of_code/problems/y2015/d07.py
+++ advent_of_code/problems/y2015/d07.py
@@ -55,161 +55,212 @@ Now, take the signal you got on wire a, override wire b to that signal, and rese
 
 import collections
 import re
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum, unique
-from typing import DefaultDict, Dict, Iterable, List, Optional, Set, Union, Any
+from typing import DefaultDict, Dict, List, Optional, Set, Union, Iterable, Callable, MutableMapping
+
+
+Wire = str
+Storage = Dict[Wire, int]
 
 
 @unique
-class OperandTypeEnum(Enum):
-    """ Тип операнда (переменная или константа) """
-    literal = 'literal'
-    wire = 'wire'
-
-
-@unique
-class OperationCodeEnum(Enum):
-    """ Код операции """
-    op_and = 'AND'
+class OperationName(Enum):
+    """ Название операции """
     op_assign = 'ASSIGN'
+    op_and = 'AND'
     op_lshift = 'LSHIFT'
     op_not = 'NOT'
     op_or = 'OR'
     op_rshift = 'RSHIFT'
 
 
+# Коды операций для присвоения одного значения сигнала проводника другому
+AssignOperations: MutableMapping[OperationName, Callable] = {
+    OperationName.op_assign: lambda x, _: x,
+}
+
+# Коды операций с двумя операндами
+BinaryOperations: MutableMapping[OperationName, Callable] = {
+    OperationName.op_and: lambda x, y: x & y,
+    OperationName.op_lshift: lambda x, y: x << y,
+    OperationName.op_or: lambda x, y: x | y,
+    OperationName.op_rshift: lambda x, y: x >> y,
+}
+
+# Коды операций с одним операндом
+UnaryOperations: MutableMapping[OperationName, Callable] = {
+    OperationName.op_not: lambda x, _: 2 ** 16 - (x + 1),
+}
+
+
 @unique
-class OperationTypeEnum(Enum):
-    """ Классификация операций по количеству операндов """
-    assign = 'assign'
-    unary = 'unary'
-    binary = 'binary'
+class OperandType(Enum):
+    """ Тип операнда (переменная или константа) """
+    literal = 'literal'
+    wire = 'wire'
 
 
-OperandValue = Union[str, int]
+OperandValue = Union[int, Wire]
 
 
-@dataclass(frozen=True)
 class Operand:
     """ Описание операнда """
-    op_type: OperandTypeEnum
-    value: OperandValue
+
+    def __init__(self, type: OperandType, value: OperandValue) -> None:
+        self.type: OperandType = type
+        self._value: OperandValue = value
+
+    def value(self, storage: Storage) -> Optional[int]:
+        """ Возвращает фактическое значение операнда """
+        if self.type == OperandType.literal:
+            return self._value
+        return storage[self._value]
+
+    def wire(self) -> Optional[str]:
+        """ Возвращает название проводника (если есть) """
+        return self._value if self.type == OperandType.wire else None
+
+    @classmethod
+    def from_str(cls, value: Optional[str]) -> Optional['Operand']:
+        """ Возвращает операнд исходя из строкового представления """
+
+        if not value:
+            return None
+
+        if value.isdigit():
+            return cls(type=OperandType.literal, value=int(value))
+
+        return cls(type=OperandType.wire, value=value)
 
 
 @dataclass(frozen=True)
-class Command:
-    """ Описание команды """
-    left: Operand
-    right: Optional[Operand]
-    operation: OperationCodeEnum
-    result: str
-    op_type: OperationTypeEnum
+class Command(ABC):
+    """ Команда на изменение сигнала проводников """
 
+    result_wire: Wire
+    first_operand: Operand
+    second_operand: Optional[Operand] = None
 
-def _bitwise_complement(value: int, bit_length: int = 16) -> int:
-    """ Возвращает логическое дополнение до 1 """
-    power_of_2: int = 2 ** bit_length
-    return power_of_2 - (value + 1)
+    @abstractmethod
+    def execute(self, storage: Storage) -> int:
+        """ Выполнение команды """
+        raise NotImplementedError()
 
+    @property
+    def first_wire(self) -> Optional[Wire]:
+        """ Возвращает название первого проводника """
+        return self.first_operand.wire()
 
-def _execute(cmd: Command, state: Dict[str, Any]) -> Dict[str, Any]:
-    """ Выполнение команды """
+    @property
+    def second_wire(self) -> Optional[Wire]:
+        """ Возвращает название второго проводника """
+        return self.second_operand.wire() if self.second_operand else None
 
-    def get_operand_value(operand: Operand) -> int:
-        """ Возвращает значение операнда команды """
+    @classmethod
+    def build(
+            cls,
+            result_wire: str,
+            first_operand: str,
+            second_operand: Optional[str] = None,
+    ) -> 'Command':
+        """ Возвращает команду в структурированном виде """
 
-        if operand.op_type == OperandTypeEnum.literal:
-            return operand.value
-
-        try:
-            return state[operand.value]
-        except LookupError as error:
-            raise ValueError(f'Reference to undefined wire {operand.value}') from error
-
-    def assign_operation() -> int:
-        """ Выполнение команды присваивания """
-        return get_operand_value(cmd.left)
-
-    def unary_operation() -> int:
-        """ Выполнение команды из одного операнда """
-        value = get_operand_value(cmd.left)
-        if cmd.operation == OperationCodeEnum.op_not:
-            return _bitwise_complement(value)
-        raise ValueError('Unknown operation')
-
-    def binary_operation() -> int:
-        """ Выполннение команды из двух операндов """
-
-        left_value = get_operand_value(cmd.left)
-        right_value = get_operand_value(cmd.right)
-
-        if cmd.operation == OperationCodeEnum.op_and:
-            return left_value & right_value
-        if cmd.operation == OperationCodeEnum.op_or:
-            return left_value | right_value
-        if cmd.operation == OperationCodeEnum.op_lshift:
-            return left_value << right_value
-        if cmd.operation == OperationCodeEnum.op_rshift:
-            return left_value >> right_value
-
-        raise ValueError('Unknown operation')
-
-    result = {
-        OperationTypeEnum.assign: assign_operation,
-        OperationTypeEnum.unary: unary_operation,
-        OperationTypeEnum.binary: binary_operation,
-    }[cmd.op_type]()
-
-    return {**state, **{cmd.result: result}}
-
-
-def _parse_operand(value: str) -> Operand:
-    """ Возвращает значение операнда """
-    if value.isdigit():
-        return Operand(op_type=OperandTypeEnum.literal, value=int(value))
-    return Operand(op_type=OperandTypeEnum.wire, value=value)
-
-
-def _parse_input(string: str) -> Command:
-    """ Возвращает сформированную команду исходя из строкового представления """
-
-    binary_operation_template = r'(\w+)\s+(AND|OR|LSHIFT|RSHIFT)\s+(\w+) -> (\w+)'
-    if (match := re.match(binary_operation_template, string)) is not None:
-        return Command(
-            left=_parse_operand(match.group(1)),
-            operation=OperationCodeEnum(match.group(2)),
-            right=_parse_operand(match.group(3)),
-            result=match.group(4),
-            op_type=OperationTypeEnum.binary,
+        return cls(
+            result_wire=result_wire,
+            first_operand=Operand.from_str(first_operand),
+            second_operand=Operand.from_str(second_operand),
         )
 
-    unary_operation_template = r'(NOT)\s+(\w+) -> (\w+)'
-    if (match := re.match(unary_operation_template, string)) is not None:
-        return Command(
-            left=_parse_operand(match.group(2)),
-            operation=OperationCodeEnum(match.group(1)),
-            right=None,
-            result=match.group(3),
-            op_type=OperationTypeEnum.unary,
-        )
+    @staticmethod
+    def _value(self, operand: Operand, storage: Storage) -> int:
+        """ Возвращает фактическое значение операнда """
 
-    assign_operation_template = r'(\w+) -> (\w+)'
-    if (match := re.match(assign_operation_template, string)) is not None:
-        return Command(
-            left=_parse_operand(match.group(1)),
-            operation=OperationCodeEnum.op_assign,
-            right=None,
-            result=match.group(2),
-            op_type=OperationTypeEnum.assign,
-        )
 
-    raise ValueError(f'Wrong command: {string}')
+class AndCommand(Command):
+    """ Обработка команды AND """
+
+    def execute(self, storage: Storage) -> int:
+        """ Выполнение команды """
+        first, second = self.first_operand.value(storage), self.second_operand.value(storage)
+        return first & second
+
+
+class AssignCommand(Command):
+    """ Обработка  """
+
+    def execute(self, storage: Storage) -> int:
+        """ Выполнение команды """
+        return self.first_operand.value(storage)
+
+
+class LShiftCommand(Command):
+    """ Обработка команды LSHIFT """
+
+    def execute(self, storage: Storage) -> int:
+        """ Выполнение команды """
+        first, second = self.first_operand.value(storage), self.second_operand.value(storage)
+        return first << second
+
+
+class NotCommand(Command):
+    """ Обработка команды NOT """
+
+    def execute(self, storage: Storage) -> int:
+        """ Выполнение команды """
+        first = self.first_operand.value(storage)
+        return 2 ** 16 - (first + 1)
+
+
+class OrCommand(Command):
+    """ Обработка команды OR """
+
+    def execute(self, storage: Storage) -> int:
+        """ Выполнение команды """
+        first, second = self.first_operand.value(storage), self.second_operand.value(storage)
+        return first | second
+
+
+class RShiftCommand(Command):
+    """ Обработка команды RSHIFT """
+
+    def execute(self, storage: Storage) -> int:
+        """ Выполнение команды """
+        first, second = self.first_operand.value(storage), self.second_operand.value(storage)
+        return first >> second
+
+
+def _command_factory(
+        operation: str,
+        result_wire: str,
+        first_operand: str,
+        second_operand: Optional[str] = None,
+) -> Command:
+    """ Фабрика создания команд """
+
+    name = OperationName((operation or '').strip().upper())
+
+    command_cls = {
+        OperationName.op_assign: AssignCommand,
+        OperationName.op_and: AndCommand,
+        OperationName.op_lshift: LShiftCommand,
+        OperationName.op_not: NotCommand,
+        OperationName.op_or: OrCommand,
+        OperationName.op_rshift: RShiftCommand,
+    }[name]
+
+    return command_cls.build(
+        result_wire=result_wire,
+        first_operand=first_operand,
+        second_operand=second_operand,
+    )
 
 
 def _topological_sort(commands: List[Command], destination: str) -> List[Command]:
     """ Топологическая сортировка команд """
 
-    indexed_commands = {cmd.result: cmd for cmd in commands}
+    indexed_commands = {cmd.result_wire: cmd for cmd in commands}
     graph: DefaultDict[str, Set[str]] = collections.defaultdict(set)
     result = []
     seen = set()
@@ -223,10 +274,10 @@ def _topological_sort(commands: List[Command], destination: str) -> List[Command
 
     # Построение графа зависимостей
     for result_wire, cmd in indexed_commands.items():
-        if cmd.left is not None and cmd.left.op_type == OperandTypeEnum.wire:
-            graph[result_wire].add(cmd.left.value)
-        if cmd.right is not None and cmd.right.op_type == OperandTypeEnum.wire:
-            graph[result_wire].add(cmd.right.value)
+        if cmd.first_wire:
+            graph[result_wire].add(cmd.first_wire)
+        if cmd.second_wire:
+            graph[result_wire].add(cmd.second_wire)
 
     recursive_helper(destination)
     result.append(indexed_commands[destination])
@@ -234,16 +285,64 @@ def _topological_sort(commands: List[Command], destination: str) -> List[Command
     return result
 
 
-def first_task(strings: Iterable[str], wire: str = 'a') -> int:
+class Simulator:
+    """ Имитатор выполнения команд прохождения сигнала """
+
+    def __init__(self, storage: Storage, commands: List[Command]) -> None:
+        self.storage: Storage = storage
+        self.commands: List[Command] = commands
+
+    def run(self, result_wire: Wire) -> int:
+        """ Выполнение команд прохождения сигнала """
+
+        for cmd in _topological_sort(self.commands, destination=result_wire):
+            self.storage[cmd.result_wire] = cmd.execute(self.storage)
+
+        return self.storage[result_wire]
+
+
+def _parse_input(string: str) -> Command:
+    """ Возвращает сформированную команду исходя из строкового представления """
+
+    binary_keywords = '|'.join(keyword.value for keyword in BinaryOperations)
+    unary_keywords = '|'.join(keyword.value for keyword in UnaryOperations)
+
+    binary_operation_template = r'(\w+)\s+({})\s+(\w+) -> (\w+)'.format(binary_keywords)
+    if (match := re.match(binary_operation_template, string)) is not None:
+        return _command_factory(
+            first_operand=match.group(1),
+            operation=match.group(2),
+            second_operand=match.group(3),
+            result_wire=match.group(4),
+        )
+
+    unary_operation_template = r'({})\s+(\w+) -> (\w+)'.format(unary_keywords)
+    if (match := re.match(unary_operation_template, string)) is not None:
+        return _command_factory(
+            first_operand=match.group(2),
+            operation=match.group(1),
+            result_wire=match.group(3),
+        )
+
+    assign_operation_template = r'(\w+) -> (\w+)'
+    if (match := re.match(assign_operation_template, string)) is not None:
+        return _command_factory(
+            first_operand=match.group(1),
+            operation=OperationName.op_assign.value,
+            result_wire=match.group(2),
+        )
+
+    raise ValueError(f'Wrong command: {string}')
+
+
+def first_task(strings: Iterable[str], result_wire: str) -> int:
     """ Решение первой задачи """
 
-    wire_state: Dict[str, int] = {}
+    storage = {}
 
-    # Получение и формирование команд
+    # Формирование списка команд
+    # Для дальнейшей обработки необходимо считать все команды
     commands = [_parse_input(string) for string in strings]
 
-    # Выполнение команд
-    for command in _topological_sort(commands=commands, destination=wire):
-        wire_state = _execute(command, state=wire_state)
-
-    return wire_state[wire]
+    # Имитация прохождения сигнала через контакты
+    return Simulator(storage, commands).run(result_wire=result_wire)
